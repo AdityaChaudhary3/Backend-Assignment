dfe924c4bd817246cda5f9d441613bbd1e9c7bea
AdityaChaudhary3 <adityac2356@gmail.com>
2024-01-31 01:23:59 +0530

    Add video upload functionality and update video details

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -15,26 +15,118 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+
+    const videoLocalPath = req.files?.videoFile[0]?.path
+
+    const thumbnailLocalPath = req.files?.thumbnail[0]?.path
+
+    if(!videoLocalPath || !thumbnailLocalPath){
+        throw new ApiError(400, "video file and thumbnail are required.")
+    }
+
+    const videoFile = await uploadOnCloudinary(videoLocalPath)
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+
+    if(!thumbnail){
+        throw new ApiError(500, "Error uploading video file or thumbnail.")
+    }
+
+    // console.log("video response",videoFile)
+
+    const video = await Video.create({
+        title,
+        description,
+        videoFile: videoFile.url,
+        thumbnail: thumbnail.url,
+        duration: videoFile.duration,
+        owner: req.user._id
+    })
+
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video created successfully."))
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+
+    if(!videoId){
+        throw new ApiError(400, "video not found.")
+    }
+
+    const video = await Video.findById(videoId)
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video retrieved successfully."))
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
 
+    if(!videoId){
+        throw new ApiError(400, "video not found.")
+    }
+
+    const video = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $set: {
+                title: req.body?.title,
+                description: req.body?.description,
+                // thumbnail: req.body?.thumbnail
+            }
+        }
+        
+    )
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video updated successfully."))
+
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: delete video
+
+    if(!videoId){
+        throw new ApiError(400, "video not found.")
+    }
+
+    await Video.findByIdAndDelete(videoId)
+
+    return res
+    .status(200)
+    .json(200, {}, "Video deleted.")
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+
+    if(!videoId){
+        throw new ApiError(400, "video not found.")
+    }
+
+    const video = await Video.findByIdAndUpdate(
+        videoId,
+        [
+            {
+                $set: {
+                    isPublished: {
+                    $eq: [false, "$isPublished"]
+                    }
+                }
+            }
+        ]
+    )
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Toggle video status successfully."))
 })
 
 export {
